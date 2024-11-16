0eafa577b45138864c514d81a5f42b618e27f11f
Lucas Rocha <ldrocha16@gmail.com>
2024-11-16 11:39:29 -0300

    Separando header da view home para reutilização em outras pag.

--- trabalho2/views/home.php
+++ trabalho2/views/home.php
@@ -9,31 +9,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-<header class="bg-dark text-white p-3">
-    <div class="container d-flex justify-content-between align-items-center">
-        <div class="logo">
-            <img id="img_logo" src="./assets/img/decoracaoLogo.png" alt="Logo Decora Fácil" style="max-height: 50px;">
-            <a id="logo" class="text-white fs-3 ms-2">Decora Fácil</a>
-        </div>
-        <div class="search d-flex">
-            <input type="text" class="form-control me-2" placeholder="Pesquisar decoração...">
-            <button class="btn btn-primary">Buscar</button>
-        </div>
-        <div class="login">
-            <?php
-            if (isset($_SESSION['user'])) { // variável está declarada e <> de null
-                $nomes = explode(' ', $_SESSION['user']['name']);
-                echo "<span class='text-white'>Olá, " . htmlspecialchars($nomes[0]) . "</span>";
-                echo "<a href='index.php?action=logout' class='text-white ms-3'>Sair</a>";
-                echo "<a href='#' class='btn btn-primary text-white ms-3'><i class='fa-solid fa-cart-shopping'></i> Carrinho</a>";
-            } else {
-                echo "<a href='index.php?action=login' class='btn btn-primary text-white'>Login</a>";
-            }
-            ?>
-        </div>
-    </div>
-</header>
-
+<?php
+include('header.php');
+?>
 <nav class="bg-light py-2">
     <div class="container">
         <ul class="nav justify-content-center">
